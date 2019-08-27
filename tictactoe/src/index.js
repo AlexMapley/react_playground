3cf853b0e23b81bd82db500fcd6c8f1b20197557
AlexMapley <alexmapley@gmail.com>
2019-08-27 11:46:43 -0400

    add win condition to tictactoe

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -38,7 +38,14 @@ function Square(props) {
     }
   
     render() {
-      const status = 'Next player: '.concat(this.state.xIsNext ? 'X' : 'O');
+      let status = 'Next player: '.concat(this.state.xIsNext ? 'X' : 'O');
+
+      const winner = calculateWinner(this.state.squares);
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
   
       return (
         <div>
@@ -77,6 +84,26 @@ function Square(props) {
         </div>
       );
     }
+  }
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
   }
   
   // ========================================
