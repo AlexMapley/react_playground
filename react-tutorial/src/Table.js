d98503378aaf7004c5f03b71dfa611fac452045b
AlexMapley <alexmapley@gmail.com>
2019-08-28 15:29:00 -0400

    add delete button to employee list

--- react-tutorial/src/Table.js
+++ react-tutorial/src/Table.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react'
 
 class Table extends Component {
   render() {
-    const { characterData } = this.props
+    const { characterData, removeCharacter } = this.props
 
     return (
       <table>
         <TableHeader />
-        <TableBody characterData={characterData} />
+        <TableBody characterData={characterData} removeCharacter={removeCharacter} />
       </table>
     )
   }
@@ -19,6 +19,9 @@ const TableBody = props => {
       <tr key={index}>
         <td>{row.name}</td>
         <td>{row.job}</td>
+        <td>
+    <button onClick={() => props.removeCharacter(index)}>Delete</button>
+  </td>
       </tr>
     )
   })
