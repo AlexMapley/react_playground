0aa5870db11ceed75a0cad7ca4f5a1ff0a4248c6
AlexMapley <alexmapley@gmail.com>
2019-08-28 15:26:40 -0400

    embed character data into state, add removeCharacter

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
 import Table from './Table'
 
+// class App extends Component {
+
+// }
+
 class App extends Component {
-  render() {
-    const characters = [
+  state = {
+    characters: [
       {
         name: 'Charlie',
         job: 'Janitor',
@@ -21,13 +25,27 @@ class App extends Component {
         job: 'Bartender',
       },
     ]
-
+  }
+  render() {
+    const { characters } = this.state
+  
     return (
       <div className="container">
-        <Table characterData={characters} />
+        <Table characterData={characters} removeCharacter={this.removeCharacter} />
       </div>
     )
   }
+
+  removeCharacter = index => {
+    const { characters } = this.state
+  
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index
+      }),
+    })
+  }
 }
+
 
 export default App
